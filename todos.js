9b476d60d578324bd0a0238c83ce6b56d5cdd22f
Jonathan Mwaniki <idolearndaily@gmail.com>
2023-04-18 15:01:28 +0300

    author edit

--- todos.js
+++ todos.js
@@ -7,58 +7,68 @@ const todos = [
 const todos_container = document.querySelector(".todos");
 
 
-const createTodo = (todo)=>{
+// creating a single todo
+const createTodo = (todo) => {
+      //creating a div
       const todo_container = document.createElement("div");
+      //adding a class to the new dic
       todo_container.classList.add("todo");
 
       const todo_span = document.createElement("span");
       todo_span.innerText = todo.title;
 
       const check_complete = document.createElement("input");
+      //setting attributes
       check_complete.setAttribute("type", "checkbox");
 
       const delete_btn = document.createElement("button");
       delete_btn.id = todo.id;
       delete_btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
-
+      //appending all elements to a todo container
       todo_container.append(todo_span, check_complete, delete_btn)
 
       return todo_container
 }
 
-
-const appendTodos = (todos)=>{
-      todos.map((todo)=>{
+// function to append all todos
+const appendTodos = (todos) => {
+      todos.map((todo) => {
             todos_container.appendChild(createTodo(todo))
       })
 }
 
-
+//initial load of all the todos
 appendTodos(todos)
 
 
 const submit_btn = document.getElementById("btn");
 const input_ele = document.getElementById("todo-input")
 
-const handleSubmit = ()=>{
-      
-      if(input_ele.value.trim() !== ""){
+
+//submit new todo handler
+const handleSubmit = () => {
+
+      if (input_ele.value.trim() !== "") {
             const new_todo = { id: new Date().toISOString(), title: input_ele.value, is_complete: false }
-      todos.unshift(new_todo)
-      todos_container.innerHTML = ""
-      input_ele.value = ""
-      appendTodos(todos)
+            //adding the todo as the first element
+            todos.unshift(new_todo)
+            todos_container.innerHTML = ""
+            input_ele.value = ""
+
+            //updating the ui to include  the new todo
+            appendTodos(todos)
       }
 }
 
+
+//listening for a click event from the button submit
 submit_btn.addEventListener("click", handleSubmit)
 
 
 
 
-
-
-todos_container.addEventListener("click", function(e){
+//deleting a todo   -- the best way is to delete it from the array and then update the ui/document
+todos_container.addEventListener("click", function (e) {
       const item = e.target;
 
       if (item.matches("span")) {
